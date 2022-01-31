bfd018c84ef6fdfc1c7e67ef6adf9d4feb01b5b1
Shanjida Jim <88015050+Shanjida03@users.noreply.github.com>
2022-01-31 17:30:59 +0600

    Add files via upload

---
+++ Task1.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Task 1</title>
+</head>
+<body>
+    
+
+<h1> Registration Form </h1>
+<hr> </hr>
+<table>
+<tr>
+<td> First Name :</td>
+   <td>  <input type = "text" Name ="name"> </td>
+</tr>
+
+<tr>
+   <td> Last Name : </td>
+   <td> <input type = "text" Name = "name"></td>
+</tr>
+
+<tr>
+   <td> Age : </td>
+   <td> <input type="text" Name = "name"> </td>
+</tr>
+
+<tr>
+    <td>Designation</td>
+    <td> 
+        <input type="radio" Name="desig">Junior Programmer
+        <input type="radio" Name="desig">Senior programmer
+        <input type="radio" Name="desig">Project Lead
+</td>
+</tr>
+<tr>
+    <td>
+        Preferred language
+</td>
+<td>
+    <input type="checkbox">JAVA
+    <input type="checkbox">PHP
+    <input type="checkbox">C++
+</td>
+</tr>
+
+<tr>
+    <td> Email :</td>
+    <td>
+        <input type="text">
+</td>
+</tr>
+<tr>
+    <td>Password</td>
+    <td>
+        <input type="password" name="password">
+</td>
+</tr>
+<tr>
+    <td> Please choose a file</td>
+    <td>
+        <input type="file" id = "myfile"></td>
+</tr>
+<tr>
+    <td>
+<button type = "submit" value ="Submit">Submit </button>
+<button type= "reset" value="Reset">Reset</button>
+</td>
+</tr>
+</table>
+</body>
+</html>
